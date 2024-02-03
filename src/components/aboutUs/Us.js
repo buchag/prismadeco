06380046fe23d13ec4ec363bfc934faba9f35c8c
eziosk44 <emapais228@gmail.com>
2024-02-03 15:20:32 -0300

    MOdificación texto

--- src/components/aboutUs/Us.js
+++ src/components/aboutUs/Us.js
@@ -12,7 +12,7 @@ const Us = () => {
                 <div className="absolute inset-0 bg-black opacity-50"></div>
                 <div className='relative z-10'>
                     <h2 className='text-center text-4xl pt-10 mt-10 pb-5 text-gray-100'>Nuestro diferencial</h2>
-                    <p className='text-base text-center pt-10 text-gray-100'>En Prismadeco nos diferenciamos por la <b>calidad de los productos y los tiempos de entrega</b>, cumpliendo los plazos establecidos<br></br> <b>contando con excelente personal en logística.</b></p>
+                    <p className='text-sm md:text-base text-center pt-5 md:pt-10 text-gray-100'>En Prismadeco nos diferenciamos por la <b>calidad de los productos y los tiempos de entrega</b>, cumpliendo los plazos establecidos<br></br> <b>contando con excelente personal en logística.</b></p>
                     <div className=' flex justify-center mt-9 '>
                         <FontAwesomeIcon icon={faClock} className='mx-4 text-2xl w-6 h-6 text-white' />
                         <FontAwesomeIcon icon={faTruck} className='mx-4 text-2xl w-6 h-6 text-white' />
