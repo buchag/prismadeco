f8c7ce40a8ce373724fcb0ca2439a3abde028c21
Luifa1210 <lfabriciom@hotmail.com>
2024-02-04 13:12:41 -0300

    Se agrego icono de la pestaña y titulo. Se modifico hoja de estilo

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>PRISMA DECO</title>
         <meta name="description" content="Prisma Deco" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/iconprisma.png" />
       </Head>
 
       <main>
