9a323999ee035763884423165c6f13b8260fdfe1
Juliromero1 <juliromero@hotmail.es>
2024-02-03 21:52:07 -0300

    "Se actualiza index"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 
-
     import Image from 'next/image'
     import { Inter } from 'next/font/google'
     import Carousel from '../components/carousel/carousel'
     import CardList from '@/components/cards/CardList'
     import Footer from "../components/footer/Footer"
     import {Component} from 'react'
+    import axios from "axios";
     const inter = Inter({ subsets: ['latin'] })
+
     
     //const inter = Inter({ subsets: ['latin'] })
       
-    const index = () => {
+  const index = () => {
     return (
       <main>
         <Carousel/>
